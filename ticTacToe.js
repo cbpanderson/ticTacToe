335913a4350e235f720c37ff3bebf0ff7754a401
Cassie Anderson <cbp.smith@gmail.com>
2021-04-29 22:21:48 -0400

    added storeScore and clearBoard

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,36 +1,76 @@
-//set up event listeners
 var clickCount = 1;
-var clickLocation = "";
 var xWins = 0;
-var yWins = 0;
+var oWins = 0;
+var currGame = ["","","","","","","","",""];
+var currNum;
 
 
     function getClickLocation(num){
+      currNum = num;
       clickLocation = document.getElementById(num);
       writeLetter(clickLocation);
     }
 
   function writeLetter(location){
-    if(location.className!=='played'){
-      if (clickCount % 2 !==0){
+    if(location.className === "gameBoxIndiv played"){
+      alert("Choose an open square.");
+    }
+    else{
+      if (clickCount % 2 !== 0){
         location.innerHTML = 'X';
         location.className += ' played';
         clickCount++;
+        storeScore('X');
       }       
       else {
       location.innerHTML = 'O';
       location.className += ' played';
       clickCount++;
+      storeScore('O');
       }
     }
   }
 
-  function addWin(playerWins){
-    document.getElementById(player+"Wins").innerHTML = playerWins;
+  function storeScore(letter){
+    currGame[currNum] = letter;
+    // checkForWin();
   }
 
+  function checkForWin(){
+    // const lines = [
+    //   [0, 1, 2],
+    //   [3, 4, 5],
+    //   [6, 7, 8],
+    //   [0, 3, 6],
+    //   [1, 4, 7],
+    //   [2, 5, 8],
+    //   [0, 4, 8],
+    //   [2, 4, 6],
+    // ];
 
-//start new game button resets everything
-//when three letters in a row, print winner at top
-//keep track of wins on right under statsticTacToe
-  
+
+
+    // //if winner:
+    // document.getElementById(winMessage).innerHTML = 'Winner!';
+    // //who won
+    // var playerWhoWon = "";
+    // addWinToScoreboard(playerWhoWon);
+
+  }
+
+  //add to this
+  // function addWinToScoreboard(player){
+
+  //   document.getElementById(player+"Wins").innerHTML = "Player "+player+":"+`{player}+"Wins"`;
+  // }
+
+  function clearBoard(){
+    currGame = ["","","","","","","","",""];
+    var i=1;
+    while (i <= 9){
+      var currDiv = document.getElementById(i); 
+      currDiv.innerHTML = "";
+      currDiv.className = "gameBoxIndiv";
+      i++;
+    }
+  }
