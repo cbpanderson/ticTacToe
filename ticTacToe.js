077d700579e3acdb2c2fd603fe299f2758b4811b
Cassie Anderson <cbp.smith@gmail.com>
2021-04-29 22:47:44 -0400

    renamed ticTacToe.html to index.html

--- ticTacToe.js
+++ ticTacToe.js
@@ -47,7 +47,14 @@ var currNum;
     //   [0, 4, 8],
     //   [2, 4, 6],
     // ];
-
+    // for (let i = 0; i < lines.length; i++) {
+    //   const [a, b, c] = lines[i];
+    //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    //     return squares[a];
+    //   }
+    // }
+    // return null;
+  
 
 
     // //if winner:
